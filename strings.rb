3e0087845d1d803ab21124411f25bf8bfbc38301
Allora Sisnroy <allsis@icloud.com>
2018-06-07 13:27:43 +0000

    Done.

---
+++ strings.rb
@@ -0,0 +1,17 @@
+puts "Hello World".upcase
+puts "Hello World".length 
+puts "Hello World".downcase
+puts "Hello World".swapcase
+puts "Hello World".reverse
+
+puts 100.to_f  
+puts "1".to_i 
+puts 55.to_s 
+
+#.to_f turns integers into deciamls 
+# a float is a deciaml 
+
+name = "Allora"
+sister_one = "Avie"
+sister_two = "Amaya"
+"Hello my name is #{name}. I have two siblings, #{sister_one} and #{sister_two}"
